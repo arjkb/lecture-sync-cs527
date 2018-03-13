56c345ac193ff7dac6891731118d66e266a8a1db
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-03-13 16:40:54 -0600

    print all lecture links

--- sync.py
+++ sync.py
@@ -5,5 +5,16 @@ def main():
     base_url = 'http://www.cs.unm.edu/~saia/classes/527-s18/'
     r = requests.get(base_url)
     print(r.status_code)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('lec/'):
+            print(base_url + href)
+
+
+
+
 if __name__ == '__main__':
     main()
